049f3ef07ba86df706aedd12e390bdeb1b98179a
Guillermo A. Amaral <g@maral.me>
2013-06-10 22:59:09 -0700

    Install all Raspberry Pi boot elf files.
    The RPi camera doesn't seem to work with the standard start and fixup elf
    files.

--- boot/rpi/rpi.mk
+++ boot/rpi/rpi.mk
@@ -11,8 +11,8 @@ RPI_LICENSE_FILE = boot/LICENCE.broadcom
 
 define RPI_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/boot/bootcode.bin $(BINARIES_DIR)/boot/bootcode.bin
-	$(INSTALL) -D -m 0644 $(@D)/boot/start.elf $(BINARIES_DIR)/boot/start.elf
-	$(INSTALL) -D -m 0644 $(@D)/boot/fixup.dat $(BINARIES_DIR)/boot/fixup.dat
+	$(INSTALL) -D -m 0644 $(@D)/boot/start*.elf $(BINARIES_DIR)/boot/
+	$(INSTALL) -D -m 0644 $(@D)/boot/fixup*.dat $(BINARIES_DIR)/boot/
 	$(INSTALL) -D -m 0644 boot/rpi/config.txt $(BINARIES_DIR)/boot/config.txt
 endef
 

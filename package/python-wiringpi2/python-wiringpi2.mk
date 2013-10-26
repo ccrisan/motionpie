a9ab90871f424ef40d10b4dc36b446e2f791986b
TinHead <razvan.lixandru@gmail.com>
2013-10-26 17:04:30 +0300

    WiringPi2 added and working. Fix for systemd execvpe redefine.

--- package/python-wiringpi2/python-wiringpi2.mk
+++ package/python-wiringpi2/python-wiringpi2.mk
@@ -11,12 +11,14 @@ PYTHON_WIRINGPI2_SITE = https://github.com/WiringPi/WiringPi2-Python.git
 PYTHON_WIRINGPI2_SITE_METHOD = git
 
 PYTHON_WIRINGPI2_DEPENDENCIES = python
+#export PYTHONPATH=/home/razvan/rpi-buildroot/output/target/usr/lib/python2.7/site-packages/
 
 define PYTHON_WIRINGPI2_BUILD_CMDS
+	(export PYTHONPATH=$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION)/site-packages/)
 	(cd $(@D); swig -python wiringpi.i)
 	(cd $(@D); CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" \
 		LDSHARED="$(TARGET_CROSS)gcc -shared" \
-		CROSS_COMPILING=yes \
+		CROSS_COMPILING=yes PYTHONPATH=$(TARGET_DIR)/usr\
 		_python_sysroot=$(STAGING_DIR) \
 		_python_srcdir=$(BUILD_DIR)/python$(PYTHON_VERSION) \
 		_python_prefix=/usr \
@@ -24,17 +26,9 @@ define PYTHON_WIRINGPI2_BUILD_CMDS
 		$(HOST_DIR)/usr/bin/python setup.py build)
 endef
 
-#ifneq ($(BR2_HAVE_DOCUMENTATION),y)
-#define PYTHON_WIRINGPI2_REMOVE_DOC
-#	rm -rf $(TARGET_DIR)/usr/lib/python*/site-packages/gevent/docs
-#endef
-#endif
-
 define PYTHON_WIRINGPI2_INSTALL_TARGET_CMDS
 	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install \
 		--prefix=$(TARGET_DIR)/usr)
-#	rm -rf $(TARGET_DIR)/usr/lib/python*/site-packages/gevent/tests
-#	$(PYTHON_WIRINGPI2_GEVENT_REMOVE_DOC)
 endef
 
 define PYTHON_WIRINGPI2_UNINSTALL_TARGET_CMDS

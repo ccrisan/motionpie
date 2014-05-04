97c9f78baf2d8df870915fd8b01414bf649770d0
Guillermo A. Amaral <g@maral.me>
2014-05-03 18:56:09 -0700

    Copy kernel into image/boot instead of moving it in.

--- board/raspberrypi/postscript.sh
+++ board/raspberrypi/postscript.sh
@@ -5,8 +5,8 @@ TARGET="${1}"
 # copy System.map
 cp ${TARGET}/../build/linux-*/System.map ${TARGET}/System.map
 
-# move kernel
-mv ${TARGET}/../images/zImage ${TARGET}/../images/boot/kernel.img
+# copy kernel
+cp ${TARGET}/../images/zImage ${TARGET}/../images/boot/kernel.img
 
 exit 0
 

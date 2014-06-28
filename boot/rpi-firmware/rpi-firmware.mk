07b53bd6b903f9464ebf301faccabcba0ed4f519
Calin Crisan <ccrisan@gmail.com>
2014-06-28 12:58:07 +0300

    dhcp, smb and other fixes

--- boot/rpi-firmware/rpi-firmware.mk
+++ boot/rpi-firmware/rpi-firmware.mk
@@ -8,11 +8,12 @@ RPI_FIRMWARE_VERSION = 462f3e3f
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
+BR2_PACKAGE_RPI_FIRMWARE_BOOT = '_x'
 
 define RPI_FIRMWARE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/boot/bootcode.bin $(BINARIES_DIR)/boot/bootcode.bin
-	$(INSTALL) -D -m 0644 $(@D)/boot/start$(BR2_PACKAGE_RPI_FIRMWARE_BOOT).elf $(BINARIES_DIR)/boot/start.elf
-	$(INSTALL) -D -m 0644 $(@D)/boot/fixup$(BR2_PACKAGE_RPI_FIRMWARE_BOOT).dat $(BINARIES_DIR)/boot/fixup.dat
+	$(INSTALL) -D -m 0644 $(@D)/boot/start$(BR2_PACKAGE_RPI_FIRMWARE_BOOT).elf $(BINARIES_DIR)/boot/start_x.elf
+	$(INSTALL) -D -m 0644 $(@D)/boot/fixup$(BR2_PACKAGE_RPI_FIRMWARE_BOOT).dat $(BINARIES_DIR)/boot/fixup_x.dat
 	$(INSTALL) -D -m 0644 boot/rpi-firmware/config.txt $(BINARIES_DIR)/boot/config.txt
 endef
 
---
+++ package/motioneye/motioneye.mk
@@ -0,0 +1,42 @@
+################################################################################
+#
+# motioneye
+#
+################################################################################
+
+MOTIONEYE_VERSION = 0.1
+MOTIONEYE_SITE = $(TOPDIR)/package/motioneye
+MOTIONEYE_SITE_METHOD = local
+SRC_DIR = /media/data/projects/motioneye/
+DST_DIR = $(TARGET_DIR)/programs/motioneye
+
+define MOTIONEYE_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/S95motioneye $(TARGET_DIR)/etc/init.d/S95motioneye
+    mkdir -p $(DST_DIR)
+    cd $(SRC_DIR) && find $(SRC_DIR) \(\
+        -name '*.py' -o \
+        -name '*.sh' -o \
+        -name '*.js' -o \
+        -name '*.html' -o \
+        -name '*.css' -o \
+        -name '*.ttf' -o \
+        -name '*.woff' -o \
+        -name '*.svg' -o \
+        -name '*.eot' -o \
+        -name '*.png' -o \
+        -name '*.jpg' -o \
+        -name '*.gif' \) \
+        \! -path '$(SRC_DIR)run*' \
+        \! -path '$(SRC_DIR)conf*' \
+        | cut -c $$(echo $(SRC_DIR) | wc -c)- | xargs -I xxx cp -p --parents xxx $(DST_DIR)
+    mv $(DST_DIR)/settings_default.py $(DST_DIR)/settings.py
+    sed -i "s%os.path.abspath(os.path.join(PROJECT_PATH, 'conf'))%'/data/etc'%" $(DST_DIR)/settings.py
+    sed -i "s%os.path.abspath(os.path.join(PROJECT_PATH, 'run'))%'/data/output'%" $(DST_DIR)/settings.py
+    sed -i "s%SMB_SHARES = False%SMB_SHARES = True%" $(DST_DIR)/settings.py
+    sed -i "s%SMB_MOUNT_ROOT = '/media'%SMB_MOUNT_ROOT = '/data/media'%" $(DST_DIR)/settings.py
+    sed -i "s%WPA_SUPPLICANT_CONF = None%WPA_SUPPLICANT_CONF = '/data/etc/wpa_supplicant.conf'%" $(DST_DIR)/settings.py
+    sed -i "s%LOG_LEVEL = logging.INFO%LOG_LEVEL = logging.DEBUG%" $(DST_DIR)/settings.py
+    sed -i "s%8765%80%" $(DST_DIR)/settings.py
+endef
+
+$(eval $(generic-package))

d5be4ccab59e9d96babed63ad8e9e127394c4cff
Calin Crisan <ccrisan@gmail.com>
2014-11-23 15:12:33 +0200

    version bump to 20141123

--- package/motioneye/motioneye.mk
+++ package/motioneye/motioneye.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 MOTIONEYE_VERSION = 925e1b4
-MOTIONPIE_VERSION = 20140928
+MOTIONPIE_VERSION = 20141123
 MOTIONEYE_SITE = https://bitbucket.org/ccrisan/motioneye/get/
 MOTIONEYE_SOURCE = $(MOTIONEYE_VERSION).tar.gz
 MOTIONEYE_LICENSE = GPLv3

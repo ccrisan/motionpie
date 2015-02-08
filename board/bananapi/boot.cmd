d312c6e893d16de2cc7ff31aa396e76b59d26ad8
Calin Crisan <ccrisan@gmail.com>
2015-02-08 20:40:15 +0200

    hostname/serial number cleanups

--- board/bananapi/boot.cmd
+++ board/bananapi/boot.cmd
@@ -1,5 +1,4 @@
 setenv bootargs console=tty1 console=tty1 root=/dev/mmcblk0p2 rootwait panic=10 ${extra}
-setenv bootdelay 0
 fatload mmc 0 0x43000000 script.bin
 fatload mmc 0 0x48000000 uImage
 bootm 0x48000000

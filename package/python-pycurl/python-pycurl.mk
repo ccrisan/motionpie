eb4018550b0b6515111ad1bfce8f44a273037ff6
Calin Crisan <ccrisan@gmail.com>
2015-04-05 10:33:27 +0300

    pycurl now picks up the correct curl-config

--- package/python-pycurl/python-pycurl.mk
+++ package/python-pycurl/python-pycurl.mk
@@ -8,6 +8,7 @@ PYTHON_PYCURL_VERSION = 7.19.5.1
 PYTHON_PYCURL_SOURCE = pycurl-$(PYTHON_PYCURL_VERSION).tar.gz
 PYTHON_PYCURL_SITE = http://pypi.python.org/packages/source/p/pycurl
 PYTHON_PYCURL_SETUP_TYPE = distutils
+PYTHON_PYCURL_BUILD_OPTS = --curl-config=$(STAGING_DIR)/usr/bin/curl-config
 
 $(eval $(python-package))
 

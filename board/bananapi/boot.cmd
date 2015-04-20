01ea84b321e83169793289a901138f52bb3a202e
Calin Crisan <ccrisan@gmail.com>
2015-04-20 23:58:00 +0300

    removed repeated console=tty1 kernel argument

--- board/bananapi/boot.cmd
+++ board/bananapi/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=tty1 console=tty1 root=/dev/mmcblk0p2 rootwait panic=10 ${extra}
+setenv bootargs console=tty1 root=/dev/mmcblk0p2 rootwait panic=10 ${extra}
 fatload mmc 0 0x43000000 script.bin
 fatload mmc 0 0x48000000 uImage
 bootm 0x48000000

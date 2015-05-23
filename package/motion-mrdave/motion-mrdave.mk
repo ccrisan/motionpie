1a0bd25c65df69ba5bc32451317d5d4d380d1d78
Calin Crisan <ccrisan@gmail.com>
2015-05-23 12:22:45 +0300

    motion-mrdave can now actually be built

--- package/motion-mrdave/motion-mrdave.mk
+++ package/motion-mrdave/motion-mrdave.mk
@@ -4,17 +4,14 @@
 #
 ################################################################################
 
-MOTION_MRDAVE_SITE = $(call github,mrdave,motion,$(MOTION_MRDAVE_VERSION))
-MOTION_MRDAVE_VERSION = 5c6f4be9e6
-
-define MOTION_MMAL_BUILD_CMDS
-    cd $(@D); \
-    TOOLPREFIX="arm-linux-" TOOLPATH="$(HOST_DIR)/usr" ROOTFSPATH="$(STAGING_DIR)" cmake .; \
-    make
-endef
+MOTION_MRDAVE_VERSION = 5c6f4be
+MOTION_MRDAVE_SITE = $(call github,mr-dave,motion,$(MOTION_MRDAVE_VERSION))
+MOTION_MRDAVE_CONF_OPTS = --without-pgsql --without-sdl --without-sqlite3 --without-mysql --with-ffmpeg=$(STAGING_DIR)/usr/lib \
+        --with-ffmpeg-headers=$(STAGING_DIR)/usr/include                                     
 
 define MOTION_MRDAVE_INSTALL_TARGET_CMDS
     cp $(@D)/motion $(TARGET_DIR)/usr/bin/motion-mrdave
 endef
 
-$(eval $(generic-package))
+$(eval $(autotools-package))
+
--- package/motion/motion.mk
+++ package/motion/motion.mk
@@ -7,6 +7,8 @@
 MOTION_SITE = http://www.lavrsen.dk/svn/motion/trunk/
 MOTION_SITE_METHOD = svn
 MOTION_VERSION = r561
-MOTION_CONF_OPTS = --without-pgsql --without-sdl --without-sqlite3 --without-mysql --with-ffmpeg=$(STAGING_DIR)/usr/lib --with-ffmpeg-headers=$(STAGING_DIR)/usr/include
+MOTION_CONF_OPTS = --without-pgsql --without-sdl --without-sqlite3 --without-mysql --with-ffmpeg=$(STAGING_DIR)/usr/lib \
+        --with-ffmpeg-headers=$(STAGING_DIR)/usr/include
 
 $(eval $(autotools-package))
+

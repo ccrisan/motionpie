252ca19313db9f210e4fb8c6d9132f99f8f641cd
Calin Crisan <ccrisan@gmail.com>
2015-06-22 00:50:29 +0300

    odroidc1: u-boot is now shipped as precompiled binary, as it requires a separate toolchain

--- board/odroidc1/postscript.sh
+++ board/odroidc1/postscript.sh
@@ -7,8 +7,6 @@ BOARD_DIR=$(dirname $0)
 COMMON_DIR=$BOARD_DIR/../common
 BOOT_DIR=$TARGET/../images/boot/
 IMG_DIR=$TARGET/../images
-UBOOT_DIR=$TARGET/../build/uboot-*
-UBOOT_HOST_DIR=$TARGET/../build/host-uboot-tools-*
 
 # copy System.map
 cp $TARGET/../build/linux-*/System.map $TARGET/System.map
@@ -18,7 +16,8 @@ mkdir -p $BOOT_DIR
 
 cp $IMG_DIR/uImage $BOOT_DIR
 cp $IMG_DIR/meson8b_odroidc.dtb $BOOT_DIR
-cp $UBOOT_DIR/sd_fuse/bl1.bin.hardkernel $IMG_DIR
+cp $BOARD_DIR/bl1.bin.hardkernel $IMG_DIR
+cp $BOARD_DIR/u-boot.bin $IMG_DIR
 cp $BOARD_DIR/boot.ini $BOOT_DIR
 
 # disable software updating

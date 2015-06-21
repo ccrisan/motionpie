2d700247c302c9394a06b93d81c46f3dbee40ad4
Calin Crisan <ccrisan@gmail.com>
2015-06-21 17:38:36 +0300

    bananapi: added rtl8192cu driver & more

--- board/bananapi/boot_k3.19.cmd
+++
@@ -1,5 +0,0 @@
-setenv bootargs console=ttyS0,115200 console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10 ${extra}
-fatload mmc 0 0x46000000 uImage
-fatload mmc 0 0x49000000 sun7i-a20-bananapi.dtb
-bootm 0x46000000 - 0x49000000
-
